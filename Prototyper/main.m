132b27b9b7c98198db209e330b2abc0b9c668020
AndyQ <andy.qua@gmail.com>
2014-01-10 07:55:43 +0000

    Initial Commit

---
+++ Prototyper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prototyper
+//
+//  Created by Andy Qua on 10/01/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
